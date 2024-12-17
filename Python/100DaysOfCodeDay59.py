84a01a18b2150a5aad75fac3ed240f5c8d5b9fd9
Pratkshya <pratikshyaparajuli37@gmail.com>
2024-12-17 03:43:19 +0000

    Initial commit

--- Python/100DaysOfCodeDay59.py
+++ Python/100DaysOfCodeDay59.py
@@ -1,19 +1,10 @@
-#Palindrome - it is a word which is same front and back.
-import os,time
-def palindrome(word):
-  if word == "":
-    print("Please enter a word to check")  
-  elif word == word[::-1] :
-    print(f"The given word {word} is a Palindrome")
-    time.sleep(1)
-  else:
-    print(f"The word {word} is not  a Palindrome")  
+#Palindrome check using Recursive
 
-while True:
-  time.sleep(1)
-  os.system("clear")
-  print("\nPalindrome Checker")
-  print("-" * 20)
-  word = input("Give me a word: ").lower()
-  #Function call
-  palindrome(word)
+def palindrome(word):
+  if len(word) <= 1:
+    return True
+  if word[0] != word[-1]:
+    return False
+  return palindrome(word[1:-1])
+
+print(palindrome("racecar"))
