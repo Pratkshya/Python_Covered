6878b132eba3a405424decaebdaec8374d89215c
Pratkshya <pratikshyaparajuli37@gmail.com>
2024-12-19 15:26:11 +0000

    project_day64

--- Python/100DaysOfCodeDay64.py
+++ Python/100DaysOfCodeDay64.py
@@ -1,32 +1,62 @@
-#Concept of OOP
+#JOB DETAILS
 
-class school:
+class job:
   name = None
-  noOfstudents = None
-  established = None
+  salary = None
+  hoursWorked = None
 
-  def __init__(self, name,noOfstudents, established):
+  def __init__(self, name, salary, hoursWorked):
     self.name = name
-    self.noOfstudents = noOfstudents
-    self.established = established
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+    
+  def display(self):
+    print("\nJOBS")
+    print("-" * 6)
+    print(f"""{self.name:<15} {self.salary:^10} {self.hoursWorked:>15}""" )
 
-  def allSchool(self):
-    print(f"{self.name} was established in {self.established}")
+class doctor(job):
 
-class gvtSchool(school):
-  language = None
+  experience = None
+  speciality = None
 
-  def __init__(self,language):
-    self.name = "Jyoti School"
-    self.established = "1967"
-    self.language = language
+  def __init__(self, salary, hoursWorked, experience, speciality):
+    self.name = "Doctor"
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+    self.experience = experience
+    self.speciality = speciality
 
-school1 = school("Peace Zone", "1200", "1998")  
-school1.allSchool() 
+  def display(self):
+    print("\nJOBS")
+    print("-" * 6)
+    print(f"""{self.name:<15} {self.salary:^10} {self.hoursWorked:>15}""" )
+    print(f"{self.experience:<10} {self.speciality:>12}")  
 
-school2 = school("Garden Academy", "580", "2001")  
-school2.allSchool()  
+class teacher(job):
+  subject = None
+  position = None
 
-school3 = gvtSchool("Nepali")
-school3.allSchool()
-print(school3.language)
+  def __init__(self, salary, hoursWorked, subject, position):
+    self.name = "Teacher"
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+    self.subject = subject
+    self.position = position
+    
+  def display(self):
+    print("\nJOBS")
+    print("-" * 6)
+    print(f"""{self.name:<15} {self.salary:^10} {self.hoursWorked:>15}""" )
+    print(f"{self.subject:<10} {self.position:>20}")  
+  
+
+
+lawyer = job("Laywer", "$100,000", "40")
+lawyer.display()
+
+doc = doctor("$230,000", "47", "6", "Neuro")
+doc.display()
+
+tech = teacher("$60,000", "48+", "CompSci", "Vice.Principle")
+tech.display()
