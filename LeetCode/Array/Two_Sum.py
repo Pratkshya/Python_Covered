ddfd7ada5ce9a310c59b0633d4b351700bfa899a
Pratkshya <pratikshyaparajuli37@gmail.com>
2025-01-09 18:36:05 +0000

    Two Sum

---
+++ LeetCode/Array/Two_Sum.py
@@ -0,0 +1,19 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+class Solution:
+  def twoSum(self,nums, target):
+      checkVal = {}
+      for i, num in enumerate(nums):
+          requiredVal = target - num
+          if requiredVal in checkVal:
+              return [checkVal[requiredVal], i]
+          checkVal[num] = i
+
+nums = [2, 7, 11, 15]  
+target = 9
+solution = Solution()
+result = solution.twoSum(nums,target)
+print(result)
